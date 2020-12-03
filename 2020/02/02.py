10837c0feddbc349a69ea90bb1c92596b223370a
Besasam <besa@uber.space>
2020-12-03 12:44:41 +0100

    needs more list comprehension

--- 2020/02/02.py
+++ 2020/02/02.py
@@ -17,10 +17,8 @@ def part_2(data):
     return sum(map(idiot_shopkeeper, data))
 
 
-data = []
-with open('input.txt') as f:
-    for line in [l.split(' ') for l in f.read().splitlines()]:
-        data.append([[int(x) for x in line[0].split('-')], line[1][:-1], line[2]])
+with open('example.txt') as f:
+    data = [[[int(x) for x in line[0].split('-')], line[1][:-1], line[2]] for line in [l.split(' ') for l in f.read().splitlines()]]
 
 print(part_1(data))
 print(part_2(data))
