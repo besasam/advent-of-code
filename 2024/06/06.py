723f2773260d87b1358f9b342b492cc93ef2e014
besasam <besa@uber.space>
2024-12-06 11:04:30 +0100

    fuck it, we bruteforce

--- 2024/06/06.py
+++ 2024/06/06.py
@@ -4,6 +4,7 @@ class Tile:
         self.y = y
         self.visited = visited
         self.obstacle = obstacle
+        self.visited_count = 0
 
     def __str__(self):
         return '#' if self.obstacle else 'X' if self.visited else '.'
@@ -27,6 +28,7 @@ class Grid:
                 self.grid[y][x] = tile
                 if c == '^':
                     self.cur = tile
+                    tile.visited_count += 1
 
     def move(self) -> bool:
         try:
@@ -35,13 +37,17 @@ class Grid:
                 self.direction = (self.direction + 1) % 4
             else:
                 nex.visited = True
+                nex.visited_count += 1
                 self.cur = nex
             return True
         except KeyError:
             return False
 
+    def get_visited(self):
+        return [self.grid[y][x] for x in range(len(self.grid[0])) for y in range(len(self.grid)) if self.grid[y][x].visited]
+
     def count_visited(self):
-        return sum(self.grid[y][x].visited for x in range(len(self.grid[0])) for y in range(len(self.grid)))
+        return len(self.get_visited())
 
     def __str__(self):
         return '\n'.join(''.join(str(self.grid[y][x]) for x in range(len(self.grid[0]))) for y in range(len(self.grid)))
@@ -54,7 +60,24 @@ def part_1(data: list) -> int:
     return grid.count_visited()
 
 
+def part_2(data: list) -> int:
+    grid = Grid(data)
+    while grid.move():
+        continue
+    possible_loops = 0
+    visited_positions = grid.get_visited()
+    for tile in visited_positions:
+        print(tile.x, tile.y)
+        modified_grid = Grid(data)
+        modified_grid.grid[tile.y][tile.x].obstacle = True
+        while modified_grid.move():
+            if modified_grid.cur.visited and modified_grid.cur.visited_count > 4:  # hail mary
+                possible_loops += 1
+                break
+    return possible_loops
+
+
 with open('input.txt') as f:
     data = f.read().splitlines()
 
-print(part_1(data))
+print(part_2(data))
